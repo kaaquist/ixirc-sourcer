56e306cede82dfd41641a0414cd97f3c334a0bc7
Kasper Aaquist Johansen <kasperaaquist@gmail.com>
2020-08-07 10:45:31 +0200

    chore: added new protocol buffer messages for the releases read from ixirc api

--- src/main/scala/com/funny/Ticker.scala
+++ src/main/scala/com/funny/Ticker.scala
@@ -5,17 +5,22 @@ import java.text.SimpleDateFormat
 import java.util.Date
 
 import akka.actor.{Actor, ActorSystem, Props}
+import com.funny.release.{ReleaseMessage, Releases}
 import com.funny.utils.Config
 import com.typesafe.scalalogging.LazyLogging
 
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.{Failure, Success}
+import io.circe._
+import io.circe.parser._
+import io.circe.Decoder, io.circe.generic.auto._
 
 /**
  * Created a ticker class to trigger a call to a web api every x sec.
  */
 object Ticker extends App with LazyLogging {
+  val formatReleaseDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
   val simpleDateFormat = new SimpleDateFormat("hh:mm:ss")
   val tickerConf = Config.theConf.tickerConf
   val Tick = "tick"
@@ -25,7 +30,23 @@ object Ticker extends App with LazyLogging {
         val data = Sourcer.getData()
         data andThen {
           case Success(value) =>
-            logger.info(s"${simpleDateFormat.format(new Date(System.currentTimeMillis()))} :: $Tick  ::  ${value.body}")
+            logger.debug(s"${simpleDateFormat.format(new Date(System.currentTimeMillis()))} :: $Tick  ::  ${value.body}")
+            val jsonLine:Json = parse(value.body).getOrElse(Json.Null)
+            val cursor = jsonLine.hcursor
+            val results = cursor.downField("results").values.head
+            logger.debug(s"Data size :: ${results.size}, first element :: ${results.head}")
+            val releaseMessages = results.map(f => {
+              // Need to make sure that we have time in mills.
+              val date = new java.util.Date(f.\\("age").head.toString().toLong * 1000L)
+              logger.info(s"Name :: ${f.\\("name").head}, Age :: ${f.\\("age").head} and Human data :: ${formatReleaseDate.format(date)}")
+              ReleaseMessage(
+                  name = f.\\("name").head.toString(),
+                  releaseDateUnixTimestamp = f.\\("age").head.toString().toLong,
+                  releaseDate = formatReleaseDate.format(date)
+              )
+            }).toSeq
+            val releases = Releases(releaseMessages)
+            logger.debug(s"This here is the results :: $releases")
           case Failure(ex) =>
             val sw = new StringWriter
             ex.printStackTrace(new PrintWriter(sw))
@@ -35,7 +56,6 @@ object Ticker extends App with LazyLogging {
   }
 
   val system = ActorSystem()
-
   val tickActor = system.actorOf(Props(classOf[TickActor]))
   // Create a ticker that ticks every 1 minute.
   val cancellable = system.scheduler.scheduleWithFixedDelay(Duration.Zero, tickerConf.waitDuration.second, tickActor, Tick)
