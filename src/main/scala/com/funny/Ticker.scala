a638e4645bf06174394700dbfaab548e462261fd
Kasper Aaquist Johansen <kasperaaquist@gmail.com>
2020-08-07 11:23:22 +0200

    chore: added actor to extract data from Api request

--- src/main/scala/com/funny/Ticker.scala
+++ src/main/scala/com/funny/Ticker.scala
@@ -6,7 +6,7 @@ import java.util.Date
 
 import akka.actor.{Actor, ActorSystem, Props}
 import com.funny.release.{ReleaseMessage, Releases}
-import com.funny.utils.Config
+import com.funny.utils.{Config, WebSiteData}
 import com.typesafe.scalalogging.LazyLogging
 
 import scala.concurrent.duration._
@@ -14,13 +14,47 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.{Failure, Success}
 import io.circe._
 import io.circe.parser._
-import io.circe.Decoder, io.circe.generic.auto._
 
+import scala.concurrent.Future
+
+class ApiDataHandler extends Actor with LazyLogging {
+  val formatReleaseDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+  def receive = {
+    case WebData(data) =>
+      logger.info(s"ApiDataHandler about to extract data ...")
+      data andThen {
+        case Success(value) =>
+          val jsonLine:Json = parse(value.body).getOrElse(Json.Null)
+          val cursor = jsonLine.hcursor
+          val results = cursor.downField("results").values.head
+          logger.debug(s"Data size :: ${results.size}, first element :: ${results.head}")
+          val releaseMessages = results.map(f => {
+            // Need to make sure that we have time in mills.
+            val date = new java.util.Date(f.\\("age").head.toString().toLong * 1000L)
+            logger.debug(s"Name :: ${f.\\("name").head}, Age :: ${f.\\("age").head} and Human data :: ${formatReleaseDate.format(date)}")
+            ReleaseMessage(
+                name = f.\\("name").head.toString(),
+                releaseDateUnixTimestamp = f.\\("age").head.toString().toLong,
+                releaseDate = formatReleaseDate.format(date)
+            )
+          }).toSeq
+          val releases = Releases(releaseMessages)
+          logger.debug(s"This here is the results :: $releases")
+        case Failure(ex) =>
+          val sw = new StringWriter
+          ex.printStackTrace(new PrintWriter(sw))
+          logger.error(s"An error occurred: ${sw.toString}")
+      }
+
+    case _ => println(s"ApiDataHandler got some other message.")
+  }
+}
+
+case class WebData(webSiteData: Future[WebSiteData])
 /**
  * Created a ticker class to trigger a call to a web api every x sec.
  */
 object Ticker extends App with LazyLogging {
-  val formatReleaseDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
   val simpleDateFormat = new SimpleDateFormat("hh:mm:ss")
   val tickerConf = Config.theConf.tickerConf
   val Tick = "tick"
@@ -28,35 +62,14 @@ object Ticker extends App with LazyLogging {
     def receive = {
       case Tick =>
         val data = Sourcer.getData()
-        data andThen {
-          case Success(value) =>
-            logger.debug(s"${simpleDateFormat.format(new Date(System.currentTimeMillis()))} :: $Tick  ::  ${value.body}")
-            val jsonLine:Json = parse(value.body).getOrElse(Json.Null)
-            val cursor = jsonLine.hcursor
-            val results = cursor.downField("results").values.head
-            logger.debug(s"Data size :: ${results.size}, first element :: ${results.head}")
-            val releaseMessages = results.map(f => {
-              // Need to make sure that we have time in mills.
-              val date = new java.util.Date(f.\\("age").head.toString().toLong * 1000L)
-              logger.info(s"Name :: ${f.\\("name").head}, Age :: ${f.\\("age").head} and Human data :: ${formatReleaseDate.format(date)}")
-              ReleaseMessage(
-                  name = f.\\("name").head.toString(),
-                  releaseDateUnixTimestamp = f.\\("age").head.toString().toLong,
-                  releaseDate = formatReleaseDate.format(date)
-              )
-            }).toSeq
-            val releases = Releases(releaseMessages)
-            logger.debug(s"This here is the results :: $releases")
-          case Failure(ex) =>
-            val sw = new StringWriter
-            ex.printStackTrace(new PrintWriter(sw))
-            logger.info(s"An error occurred: ${sw.toString}")
-        }
+        logger.debug(s"${simpleDateFormat.format(new Date(System.currentTimeMillis()))} :: $Tick")
+        apiDataHandler ! WebData(data)
     }
   }
-
   val system = ActorSystem()
+
   val tickActor = system.actorOf(Props(classOf[TickActor]))
+  val apiDataHandler = system.actorOf(Props[ApiDataHandler], name = "ApiDataHandler")
   // Create a ticker that ticks every 1 minute.
   val cancellable = system.scheduler.scheduleWithFixedDelay(Duration.Zero, tickerConf.waitDuration.second, tickActor, Tick)
 
